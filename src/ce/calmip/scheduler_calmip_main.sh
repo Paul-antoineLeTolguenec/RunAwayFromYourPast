926533904db0f55973e3b5db59a1036f6cdf9807
Paul-antoineLeTolguenec <paul-antoine.le_tolguenec@ensta-bretagne.org>
2024-05-28 16:28:04 +0200

    calmip ready for other user and main

--- src/ce/calmip/scheduler_calmip_main.sh
+++ src/ce/calmip/scheduler_calmip_main.sh
@@ -2,7 +2,7 @@
 
 parent_dir=$(dirname "$(pwd)")
 
-for file in "$parent_dir"/*ppo.py; do
+for file in "$parent_dir"/*ppo*; do
     if [ -f "$file" ]; then
         cmd="bash scheduler_calmip_per_algo.sh ../$(basename "$file")"
         echo "Running: $cmd"
