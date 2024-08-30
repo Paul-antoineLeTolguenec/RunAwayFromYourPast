69c35def5d96c2abe0879b67a03ed145ccd7a22e
3730 <p21049lp@olympelogin1.bullx>
2024-08-30 14:11:37 +0200

    sac version for slurm

--- src/baselines/calmip/scheduler_calmip_baselines.sh
+++ src/baselines/calmip/scheduler_calmip_baselines.sh
@@ -2,7 +2,7 @@
 
 parent_dir=$(dirname "$(pwd)")
 
-for file in "$parent_dir"/*ppo*; do
+for file in "$parent_dir"/*sac*; do
     if [ -f "$file" ]; then
         cmd="bash scheduler_calmip_per_algo.sh ../$(basename "$file")"
         echo "Running: $cmd"
