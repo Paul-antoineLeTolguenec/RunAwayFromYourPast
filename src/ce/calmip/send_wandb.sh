f24a1804c4e763f89b081c6ff020e2431c9025eb
3730 <p21049lp@olympelogin1.bullx>
2024-09-09 19:23:30 +0200

    changed send_wandb

--- src/ce/calmip/send_wandb.sh
+++ src/ce/calmip/send_wandb.sh
@@ -12,6 +12,7 @@ if [[ "$HOSTNAME" == *"pando"* ]]; then
     export WANDB_DATA_DIR="/scratch/disc/p.le-tolguenec/"
 elif [[ "$HOSTNAME" == *"olympe"* ]]; then
     if [[ "$USER" != "p21049lp" && "$USER" != "letolgue" ]]; then
+        path_offline="/tmpdir/$USER/P_A/wandb/"
         export WANDB_DIR="/tmpdir/$USER/P_A/"
         export WANDB_CACHE_DIR="/tmpdir/$USER/P_A/"
         export WANDB_CONFIG_DIR="/tmpdir/$USER/P_A/"
@@ -19,6 +20,7 @@ elif [[ "$HOSTNAME" == *"olympe"* ]]; then
         export WANDB_RUN_DIR="/tmpdir/$USER/P_A/"
         export WANDB_DATA_DIR="/tmpdir/$USER/P_A/"
     else
+        path_offline="/tmpdir/$USER/wandb/"
         export WANDB_DIR="/tmpdir/$USER/"
         export WANDB_CACHE_DIR="/tmpdir/$USER/"
         export WANDB_CONFIG_DIR="/tmpdir/$USER/"
@@ -28,75 +30,28 @@ elif [[ "$HOSTNAME" == *"olympe"* ]]; then
     fi
 fi
 
-
-# # create repository if it does not exist
-# mkdir -p "/tmpdir/$USER/wandb_export_dir/"
-# mkdir -p "/tmpdir/$USER/wandb_export_cache_dir/"
-# mkdir -p "/tmpdir/$USER/wandb_export_config_dir/"
-# mkdir -p "/tmpdir/$USER/wandb_export_artifacts_dir/"
-# mkdir -p "/tmpdir/$USER/wandb_export_run_dir/"
-# mkdir -p "/tmpdir/$USER/wandb_export_data_dir/"
-
-# Vérifier si au moins un argument est fourni
-if [ "$#" -lt 1 ]; then
-    echo "Usage: $0 <algorithm_name1> <algorithm_name2> ..."
-    exit 1
-fi
-
-ALGO_NAMES=("$@")
-
-# Fonction pour extraire la valeur d'un argument spécifique dans wandb-metadata.json
-extract_arg_value() {
-    local json_file=$1
-    local arg_name=$2
-    grep -A1 "\"--$arg_name\"" "$json_file" | tail -n1 | awk -F'"' '{print $2}'
-}
-
-# Fonction pour vérifier si un fichier contient au moins un des algorithmes
-contains_algo() {
-    local json_file=$1
-    for algo in "${ALGO_NAMES[@]}"; do
-        if grep -q "$algo" "$json_file"; then
-            return 0
-        fi
-    done
-    return 1
-}
-
+# get status 
 get_run_status() {
     local json_file=$1
-    echo "Extraction du statut du run depuis $json_file"
-    local status=$(grep -oP '"state":\s*"\K[^"]+' "$json_file")
-    echo "$status"
+    # echo "Extraction du statut du run depuis $json_file"
+    run_status=$(grep -oP '"state":\s*"\K[^"]+' "$json_file")
+    echo $run_status
 }
 
-count=0
+# Vérifier si le chemin défini par path_offline existe
+if [ -d "$path_offline" ]; then
+    echo "Recherche des dossiers contenant 'offline' dans $path_offline..."
 
-# Parcourir les dossiers commençant par "offline" dans le répertoire défini
-for DIR in "$path_offline"offline*; do
-    # Vérifier si c'est un dossier
-    if [ -d "$DIR" ]; then
-        # Chemin vers le fichier wandb-metadata.json
-        METADATA_FILE="$DIR/files/wandb-metadata.json"
-        SYNC_MARKER="$DIR/*.synced"
-        
-        # Vérifier si le fichier existe et n'a pas déjà été synchronisé
-        if [ -f "$METADATA_FILE" ]; then
-            # Vérifier si le state est "finished"
-            if [ "$(get_run_status "$METADATA_FILE")" == "finished" ]; then
-                # Extraire l'env_id
-                ENV_ID=$(extract_arg_value "$METADATA_FILE" "env_id")
-                count=$((count + 1))
-                echo "Synchronizing $DIR with env_id: $ENV_ID..."
-                wandb sync "$DIR"
-                if [ $? -eq 0 ]; then
-                    echo "Successfully synchronized $DIR with env_id: $ENV_ID"
-                else
-                    echo "Failed to synchronize $DIR with env_id: $ENV_ID"
-                fi
-            fi
+    # Utiliser une boucle for pour parcourir les dossiers
+    for dir in "$path_offline"*/; do
+        # Vérifier si le nom du dossier contient "offline"
+        if [[ "$dir" == *offline* ]]; then
+            tmp_path="${dir}files/wandb-metadata.json"
+            RUN_STATUS=$(get_run_status "$tmp_path")
+            echo "Statut du run : $RUN_STATUS"
+            # if [ "$RUN_STATUS" == "finished" ]; then
         fi
-    fi
-done
-
-echo "Number of directories synchronized: $count"
+    done
+else
+    echo "Le chemin $path_offline n'existe pas."
+fi
